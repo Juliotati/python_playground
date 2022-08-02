5a18cbec42eef437107923ce28c622deea71a630
_juliotati <camilo.julio77@gmail.com>
2022-08-02 21:54:55 +0100

    completed task 4

--- tasks/task_4.py
+++ tasks/task_4.py
@@ -7,17 +7,31 @@
 
 community_members = ["starving", "pinsaregood", "arth", "blazertherazer", "snow", "tess", "morne", "darthtilda"]
 
+criteria = 'a'
+
 
 def start_task4():
-    criteria = 'a'
-    members_names_containing_a = []
+    members_names_containing_criteria = []
     for member in community_members:
         if criteria in member:
-            members_names_containing_a.append(member)
+            members_names_containing_criteria.append(member)
 
-    print(f'total entries: {len(members_names_containing_a)}')
-    print('✨ 🏁 ️task 4 completed')
+    remove_members_with_criteria(community_members)
+
+    print(f'filtered_members: {members_names_containing_criteria}')
+    print(f'total entries: {len(members_names_containing_criteria)}')
+    print('----------')
+    print(f'remaining_members: {community_members}')
+
+
+
+def remove_members_with_criteria(remaining_members: [str]):
+    for member in remaining_members:
+        if criteria in member:
+            community_members.remove(member)
+            remove_members_with_criteria(community_members)
 
 
 if __name__ == '__main__':
     start_task4()
+    print('✨ 🏁 ️task 4 completed')

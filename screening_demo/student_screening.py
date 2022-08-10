42cf3573423284b1dd028b2ec08bb2be2ba2a3c8
_juliotati <camilo.julio77@gmail.com>
2022-08-10 21:24:13 +0100

    make class properties private

--- screening_demo/student_screening.py
+++ screening_demo/student_screening.py
@@ -4,29 +4,29 @@ from screening_demo.student import Student
 
 
 class StudentsScreening:
-    all_students: list = [Student]
-    closing_time: int = 12
-    min_temperature: int = 35
-    max_temperature: int = 42
-    normal_temperature: int = 38
-    campus_capacity: int = 300
-    total_students = range(500)
-    last_student_arrival: int = 0
+    _all_students: list = [Student]
+    _closing_time: int = 12
+    _min_temperature: int = 35
+    _max_temperature: int = 42
+    _normal_temperature: int = 38
+    _campus_capacity: int = 300
+    _total_students = range(500)
+    _last_student_arrival: int = 0
 
     def __init__(self):
         # nothing here
         pass
 
     def start_screening(self):
-        self.reset(self)
+        self._reset(self)
         print('------------------------------------------------------------------------------------')
         print('Starting screening')
-        for index in self.total_students:
-            if self.campus_is_full() and self.should_close_campus():
+        for index in self._total_students:
+            if self._campus_is_full() and self._should_close_campus():
                 student_arrival_time: int = random.randint(0, 12)
-                student_temperature: int = self.get_student_temperature()
-                if self.is_after_closing_time(student_arrival_time) and self.has_high_temp(student_temperature):
-                    self.all_students.append(
+                student_temperature: int = self._get_student_temperature()
+                if self._is_after_closing_time(student_arrival_time) and self._has_high_temp(student_temperature):
+                    self._all_students.append(
                         Student(
                             f'Student {index}',
                             student_temperature,
@@ -34,11 +34,11 @@ class StudentsScreening:
                             student_arrival_time,
                         )
                     )
-                    self.admitted(index, student_arrival_time, student_temperature)
+                    self._admitted(index, student_arrival_time, student_temperature)
                     continue
 
-                self.last_student_arrival = student_arrival_time
-                self.all_students.append(
+                self._last_student_arrival = student_arrival_time
+                self._all_students.append(
                     Student(
                         f'Student {index}',
                         student_temperature,
@@ -46,56 +46,56 @@ class StudentsScreening:
                         student_arrival_time,
                     ),
                 )
-                self.not_admitted(index, student_arrival_time, student_temperature)
+                self._not_admitted(index, student_arrival_time, student_temperature)
 
-        self.get_results()
+        self._get_results()
 
-    def get_results(self):
+    def _get_results(self):
         print('\n')
-        print(f'All students: {len(self.students())}')
-        print(f'Admitted students: {len(self.students(True))}')
+        print(f'All _students: {len(self._students())}')
+        print(f'Admitted _students: {len(self._students(True))}')
 
-        print(f'Students not admitted: {len(self.students(False))}')
+        print(f'Students not _admitted: {len(self._students(False))}')
         print('------------------------------------------------------------------------------------')
 
     @staticmethod
-    def not_admitted(index: int, student_arrival_time, students_temperature: int):
+    def _not_admitted(index: int, student_arrival_time, students_temperature: int):
         print(
             f'Student {index} could not enter at {student_arrival_time}, temp is {students_temperature} and is not '
-            f'admitted.')
+            f'_admitted.')
 
     @staticmethod
-    def admitted(name: int, student_arrival_time: int, students_temperature: int):
-        print(f'Student {name} enter at {student_arrival_time}, temp is {students_temperature} and is admitted.')
+    def _admitted(name: int, student_arrival_time: int, students_temperature: int):
+        print(f'Student {name} enter at {student_arrival_time}, temp is {students_temperature} and is _admitted.')
 
     @staticmethod
-    def reset(self):
-        self.last_student_arrival = 0
-        self.all_students.clear()
+    def _reset(self):
+        self._last_student_arrival = 0
+        self._all_students.clear()
 
-    def get_student_temperature(self) -> int:
-        return random.randrange(self.min_temperature, self.max_temperature)
+    def _get_student_temperature(self) -> int:
+        return random.randrange(self._min_temperature, self._max_temperature)
 
-    def is_after_closing_time(self, random_hour: int) -> bool:
-        return random_hour < self.closing_time
+    def _is_after_closing_time(self, random_hour: int) -> bool:
+        return random_hour < self._closing_time
 
-    def has_high_temp(self, random_temp) -> bool:
-        return random_temp < self.normal_temperature
+    def _has_high_temp(self, random_temp) -> bool:
+        return random_temp < self._normal_temperature
 
-    def campus_is_full(self) -> bool:
-        return len(self.students(True)) < self.campus_capacity
+    def _campus_is_full(self) -> bool:
+        return len(self._students(True)) < self._campus_capacity
 
-    def should_close_campus(self) -> bool:
-        return self.last_student_arrival < self.closing_time
+    def _should_close_campus(self) -> bool:
+        return self._last_student_arrival < self._closing_time
 
-    def students(self, admitted_status: bool = None) -> [Student]:
+    def _students(self, admitted_status: bool = None) -> [Student]:
         if admitted_status is not None:
-            return self.students_with_status(admitted_status)
-        return self.all_students
+            return self._students_with_status(admitted_status)
+        return self._all_students
 
-    def students_with_status(self, admitted_status: bool):
+    def _students_with_status(self, admitted_status: bool):
         students = []
-        for student in self.all_students:
+        for student in self._all_students:
             if student.admission_status is admitted_status:
                 students.append(student)
         return students
